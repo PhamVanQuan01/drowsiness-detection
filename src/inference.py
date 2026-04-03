54aed275b553e55a42a488c7433c592b98ccd3f6
vvdung <19.phamvanquan@gmail.com>
2026-04-03 23:33:40 +0700

    Fix: Use keras.models instead of tensorflow.keras.models

--- src/inference.py
+++ src/inference.py
@@ -5,7 +5,7 @@ from typing import Any
 import json
 
 import numpy as np
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 from src.preprocessing import prepare_eye_crop_for_model
 
